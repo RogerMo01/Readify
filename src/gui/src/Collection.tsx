368b0b39f7b35da1d169254baac5f0f162d6d7ad
Roger <morenoroger001@gmail.com>
2024-02-13 21:24:23 -0500

    fix: Remove deleted import directive

--- src/gui/src/Collection.tsx
+++ src/gui/src/Collection.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Book } from "./types/types";
-import "./Collection.css"
 import Card from "./Card";
 
 function Collection() {
