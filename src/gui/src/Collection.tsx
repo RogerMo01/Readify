aadbab471371e5780c72f68a0a0871659793f37f
Roger <morenoroger001@gmail.com>
2024-02-13 21:20:36 -0500

    feat: Add book collection view
    - Install Tailwind CSS

---
+++ src/gui/src/Collection.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Book } from "./types/types";
+import "./Collection.css"
+import Card from "./Card";
+
+function Collection() {
+    const [bookList, setBookList] = useState<Book[]>([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/list')
+          .then(response => {
+            setBookList(response.data.data);
+            console.log(`Received: ${response.data.data}`)
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }, []);
+      
+    return(
+        <div>
+            <h1 className="flex py-5 md:px-10 px-5 md:mx-20 mx-5 font-bold text-4xl text-gray-800"> Books </h1>
+            <div
+                className="flex overflow-x-scroll pb-10 hide-scroll-bar"
+            >
+                <div
+                className="flex flex-nowrap"
+                >
+                    {bookList.map((book) => (
+                        <div className="inline-block px-3">
+                            <Card book={book}/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Collection;
