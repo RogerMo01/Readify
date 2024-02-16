71a886864558a50ef0984d688e9ae9d2f394bc96
Roger <morenoroger001@gmail.com>
2024-02-16 00:44:36 -0500

    feat: Add users average rating

--- src/code/setup.py
+++ src/code/setup.py
@@ -14,13 +14,16 @@ books_file = os.path.abspath(books_file)
 ratings_file = os.path.join(parentDir, '..', 'data', 'Ratings.csv')
 ratings_file = os.path.abspath(ratings_file)
 
+ratings = []
+
+
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Save implicit ratings graph ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print('⏳ Starting save implicit ratings graph...')
 user_book_list = {}
 book_user_list = {}
 
-# Scan all books
 with open(ratings_file, newline='', encoding='utf-8') as open_ratings_file:
     csv_reader = csv.DictReader(open_ratings_file)
     
@@ -28,6 +31,8 @@ with open(ratings_file, newline='', encoding='utf-8') as open_ratings_file:
     for rating in csv_reader:
         user = rating['userID']
         book = rating['isbn']
+
+        ratings.append(rating)
 
         try:
             user_book_list[user].append(book)
@@ -55,3 +60,57 @@ print('✅ Success')
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Save users avg rating ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print('⏳ Starting save users avg ratings...')
+users_ratings = {}
+users_avg_rating = {}
+
+print('⏳ Scanning ratings...')
+for rating in ratings:
+    user  = rating['userID']
+    bookRating = rating['bookRating']
+
+    if bookRating != "0":
+        try:
+            users_ratings[user].append(int(bookRating))
+        except:
+            users_ratings[user] = [int(bookRating)]
+
+for userID, bookRatings in users_ratings.items():
+    if len(bookRatings) == 0:
+        avg = 0.0
+    else:
+        total_sum = sum(bookRatings)
+        avg = total_sum / len(bookRatings)
+    
+    avg = "{:.2f}".format(avg)
+
+    users_avg_rating[userID] = avg
+print('✅ Success')
+
+
+print('⏳ Saving avg...')
+dest_file = os.path.join(parentDir, '..', 'data', 'users_avg_rating.json')
+dest_file = os.path.abspath(dest_file)
+
+with open(users_file, newline='', encoding='utf-8') as open_users_file:
+    csv_reader = csv.DictReader(open_users_file)
+
+    for u in csv_reader:
+        userID = u['userID']
+        try:
+            temp = users_avg_rating[userID]
+        except:
+            users_avg_rating[userID] = "0.0"
+
+with open(dest_file, 'w') as json_file:
+    json.dump(users_avg_rating, json_file, indent=2)
+print('✅ Success')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+print("✅ Set Up successful")
