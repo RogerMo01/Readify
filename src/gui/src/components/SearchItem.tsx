431c73d3d62e83c324c9b08d749a31be58f23811
Roger <morenoroger001@gmail.com>
2024-02-19 20:39:51 -0500

    fix: Replace 'reviews' for 'ratings'

--- src/gui/src/components/SearchItem.tsx
+++ src/gui/src/components/SearchItem.tsx
@@ -32,7 +32,7 @@ function SearchItem({book, refreshValue, refresh}:Props){
                                 d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                         </svg>
                         <p className="text-sm text-gray-400">
-                            ({book.countRating} reviews)
+                            ({book.countRating} ratings)
                         </p>
                         <RateModal book={book} refresh={refresh} refreshValue={refreshValue}/>
                         {/* <PlusCircleIcon className="h-6 w-10 text-blue-600 mr-10 hover:cursor-pointer rounded-full hover:text-blue-500" onClick={handleAddBook} /> */}
