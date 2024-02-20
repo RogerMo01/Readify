b00855a1a5772eba5647b73e1a053a509fbbed0c
Roger <morenoroger001@gmail.com>
2024-02-20 00:17:09 -0500

    feat: Add Spinner

--- src/gui/src/components/CollectionGroup.tsx
+++ src/gui/src/components/CollectionGroup.tsx
@@ -11,9 +11,12 @@ function CollectionGroup({ refreshValue }: Props) {
   const [mainBooksList, setMainBooksList] = useState<Book[]>([]);
   const [favAuthorBooksList, setFavAuthorBooksList] = useState<Book[]>([]);
   const [favAuthor, setFavAuthor] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   // Load user library
   useEffect(() => {
+    setIsLoading(true);
+    
     axios
       .get("http://localhost:8000/api/main-recommendation")
       .then((response) => {
@@ -31,19 +34,29 @@ function CollectionGroup({ refreshValue }: Props) {
       })
       .catch((error) => {
         console.log(error);
+      })
+      .finally(() => {
+        // Ocultar el spinner cuando las solicitudes han terminado
+        setIsLoading(false);
       });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [refreshValue]);
 
-  useEffect(() => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [refreshValue]);
 
   return (
     <>
-      <Collection title="Made for you" books={mainBooksList} />
-      {favAuthorBooksList.length > 0 && <Collection title={`More from ${favAuthor}`} books={favAuthorBooksList} />}
+      {isLoading && (
+        <div className="flex items-center justify-center mt-64">
+          <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-blue-500"></div>
+        </div>
+      )}
+      {!isLoading &&
+        <>
+          <Collection title="Made for you" books={mainBooksList} />
+          {favAuthorBooksList.length > 0 && <Collection title={`More from ${favAuthor}`} books={favAuthorBooksList} />}
+        </>
+      }
     </>
   );
 }
